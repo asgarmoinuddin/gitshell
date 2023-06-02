bcee42dee346f49c678c61e71b5ea0dbc332725c
moin <moin.devops06@gmail.com>
2023-06-02 16:14:50 -0400

    Roboshop Nginx script

---
+++ mongodb.sh
@@ -0,0 +1,9 @@
+echo -e "e\[33m Copy Repo file e\[0m"
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/mongo.log
+
+echo -e "e\[33m installing mongodb e\[0m"
+yum install mongodb-org -y &>>/tmp/mongo.log
+
+echo -e "e\[33m start Service e\[0m"
+systemctl enable mongod &>>/tmp/mongo.log
+systemctl restart mongod &>>/tmp/mongo.log
