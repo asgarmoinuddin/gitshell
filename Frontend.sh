77cdab2b9a727c37a36b57585a84507b6e5043ca
moin <moin.devops06@gmail.com>
2023-06-02 15:48:34 -0400

    Roboshop Nginx script

--- Frontend.sh
+++ Frontend.sh
@@ -1,16 +1,16 @@
-echo -e "\e[33m Installing Niginx server \e[0m"
-echo -e "\e[33mInstalling Niginx server withouspace\e[0m"
+echo -e "\e[31m Installing Niginx server \e[0m"
 yum install nginx -y
 
+echo -e "\e[31m Removing Default Content \e[0m"
 rm -rf /usr/share/nginx/html/*
 
+echo -e "\e[31m Downloading and extracting content \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 #config file
 #test
-
+echo -e "\e[31m Restarting Service \e[0m"
 systemctl enable nginx
 systemctl restart nginx
